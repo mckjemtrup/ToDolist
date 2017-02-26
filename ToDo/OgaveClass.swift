5af95aeddda37ff8ca05c1f6d978de4f186726b8
Michael Kjemtrup <mckjemtrup@gmail.com>
2017-02-26 17:25:29 +0100

    class fil oprettet, tableview til opgaveadder tilføjet og alt fungerer efter hensigten

---
+++ ToDo/OgaveClass.swift
@@ -0,0 +1,15 @@
+//
+//  OgaveClass.swift
+//  ToDo
+//
+//  Created by Michael Kjemtrup on 26/02/2017.
+//  Copyright © 2017 Michael Kjemtrup. All rights reserved.
+//
+
+import Foundation
+
+class opgave {
+    var opgnavn = ""
+    var vigtighed = false
+    
+}
--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -12,23 +12,62 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var TaskTableView: UITableView!
     
+    var opgaver : [opgave]  = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        opgaver = opretOpgave()
+        
         TaskTableView.dataSource = self
         TaskTableView.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return opgaver.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Holá"
+        let opgave = opgaver[indexPath.row]
+        if opgave.vigtighed {
+            cell.textLabel?.text = "‼️\(opgave.opgnavn)"
+        } else {
+            cell.textLabel?.text = opgave.opgnavn
+        }
+        
         return cell
     }
+    
+    func opretOpgave() -> [opgave] {
+        let opgave1 = opgave()
+        
+        opgave1.opgnavn = "Gå med hunden"
+        opgave1.vigtighed = false
+        
+        
+        let opgave2 = opgave()
+        
+        opgave2.opgnavn = "Hent mælk"
+        opgave2.vigtighed = true
+        
+        let opgave3 = opgave()
+        
+        opgave3.opgnavn = "Lav aftensmad "
+        opgave3.vigtighed = false
+        
+        return [opgave1, opgave2, opgave3]
+    }
+    
+    
+    @IBAction func tilføjOpgave(_ sender: Any) {
+        performSegue(withIdentifier: "tilføjopgsegue", sender: nil )
+    }
+    
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
